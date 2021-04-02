6d3cdb0d49a61988d2c8395bd073e756728b3fbe
BogdanLyopa <bogdanlyopa@gmail.com>
2021-04-02 21:01:45 +0300

    add Section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React, { Component } from "react";
+import Section from "./components/Section";
 
+class App extends Component {
+  render() {
+    return <Section title={"Please leave feedback"}></Section>;
+  }
+}
 export default App;
